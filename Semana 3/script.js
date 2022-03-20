388b2c591a15b6f7cef530d55fc118789adbd7e9
pedroarouck <pedrohenrique.sales@hotmail.com>
2022-03-20 11:25:06 -0300

    Adicionando ex 4

--- Semana 3/script.js
+++ Semana 3/script.js
@@ -52,3 +52,25 @@ function item3Divisao(){
     var num2 = Number(document.getElementById("num2Ex3").value);
     document.getElementById("resultEx3").value = num1 / num2;   
 }
+
+function item4() {
+    let numPA = Number(document.getElementById("num1Ex4").value);
+    let numPG = Number(document.getElementById("num1Ex4").value);
+    let raiz = Number(document.getElementById("raizEx4").value);
+    let termo = 10;
+    let termoAtual;
+    let termoPG = 1;
+    respostaPA = "";
+    respostaPG = ""; 
+    for(let count=1; count<=termo ; count++){
+        numPA = numPA + raiz;
+        respostaPA += "Termo " + count + " = " + numPA +"<br>";     
+    }
+    for(let count=0; count < termo ; count++){
+        termoAtual = numPG * Math.pow(raiz, count);
+        respostaPG += "Termo " + termoPG + " = " + termoAtual +"<br>"; 
+        termoPG++;
+    }
+    document.getElementById("ex4PA").innerHTML = "PA:<br> " + respostaPA;
+    document.getElementById("ex4PG").innerHTML = "PG:<br> " + respostaPG;
+}
